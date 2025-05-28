fce0576c0f6a36c4631fb206475503d40c8a8325
rayan328 <rayan.habibeche@epitech.eu>
2025-05-28 16:44:14 +0200

    FIX Email mailer, ADD Reset password

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -42,7 +42,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('waqastravail93@gmail.com', 'Acme Mail Bot'))
+                    ->from(new Address($_ENV['MAILER_FROM_EMAIL'] ?? 'noreply@example.com', $_ENV['MAILER_FROM_NAME'] ?? 'Quiz App'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
