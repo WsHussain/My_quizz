c255a7f1f27d2f036a44d5f66e7344bed8436e2c
Waqas <waqas.hussain@epitech.eu>
2025-06-02 12:26:08 +0200

    Add quizz & question/reponse

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -7,6 +7,7 @@ use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategorieController extends AbstractController 
 {
@@ -15,6 +16,21 @@ class CategorieController extends AbstractController
     {
         return $this->render('category/index.html.twig', [
             'categories' => $categorieRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/categorie/{id}', name: 'app_categorie_show')]
+    public function show(int $id, CategorieRepository $categorieRepository): Response
+    {
+        $categorie = $categorieRepository->find($id);
+        
+        if (!$categorie) {
+            throw $this->createNotFoundException('Catégorie non trouvée');
+        }
+
+        return $this->render('category/show.html.twig', [
+            'categorie' => $categorie,
+            'questions' => $categorie->getQuestions(),
         ]);
     }
 }
